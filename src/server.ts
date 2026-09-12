1612b14f37438b0fa18f6876cb900c6d8b864a60
Manoel Henrique <manoel.hmcorrea@gmail.com>
2026-09-12 19:38:27 -0300

    feat: inicializa conexão com o banco de dados PostgreSQL

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
 import app from "./app";
+import { AppDataSource } from "./database/data-source";
 
 const PORT = 3000;
 
+AppDataSource.initialize()
+    .then(() =>{
+        console.log("Banco de dados conectado!");
+    
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
+    });
+})
+
+.catch((error) => {
+    console.error("Error ao conectar ao banco:", error);
 });
